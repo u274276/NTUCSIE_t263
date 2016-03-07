77d69adc7e778e1d43f33ed4e9a42f940c9ffc81
festiny <festinymail@gmail.com>
2016-03-07 20:16:13 +0800

    onclick event

--- SimpleUI/app/src/main/java/com/example/simpleui/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/simpleui/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.simpleui.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submit (View view)
+    {
+        Toast.makeText(this, "Hello World!!", Toast.LENGTH_LONG).show();
+
     }
 }
