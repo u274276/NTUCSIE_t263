3a2e2a9b4aebe32967466436eb11dcb28d8c85bc
festiny <festinymail@gmail.com>
2016-03-28 22:29:09 +0800

    第八堂課-哈哈哈哈哈

--- SimpleUI/app/src/main/java/com/example/simpleui/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/simpleui/OrderDetailActivity.java
@@ -1,7 +1,12 @@
 package com.example.simpleui.simpleui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,10 +18,15 @@ import org.json.JSONObject;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
+//    private String url;
+//    private String address;
+
     TextView note;
     TextView storeInfo;
     TextView menu;
     ImageView photo;
+    ImageView staticMapimageView;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +37,14 @@ public class OrderDetailActivity extends AppCompatActivity {
         storeInfo = (TextView)findViewById(R.id.storeInfoView);
         menu = (TextView)findViewById(R.id.menuView);
         photo = (ImageView)findViewById(R.id.photoView);
+        staticMapimageView = (ImageView)findViewById(R.id.staticMapimageView);
+        webView = (WebView)findViewById(R.id.webView);
+
 
         note.setText(getIntent().getStringExtra("note"));
+
+        String storeInformation = getIntent().getStringExtra("storeInfo");
+
         storeInfo.setText(getIntent().getStringExtra("storeInfo"));
 
         String menuResult = getIntent().getStringExtra("menu");
@@ -54,11 +70,130 @@ public class OrderDetailActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+//        String address = storeInformation.split(",")[1];
+
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                double[] locations = Utils.addressToLatLng(address);
+//                String debugLog = "lat:" + String.valueOf(locations[0] + "lng: " + String.valueOf(locations[1]));
+//
+//                //Log.d("debug", result);
+//                Log.d("debug", debugLog);
+//
+//            }
+//        });
+//        thread.start();
+
+
+//        不用Picasso這個網站的方法，改用Thread或是改用AsyncTask
+//        String url = getIntent().getStringExtra("photoURL");
+//        if (url != null)
+//        {
+//            Picasso.with(this).load(url).into(photo);
+//        }
+
+
+//        String url = getIntent().getStringExtra("photoURL");
+        //避免url是空值，導致APP當掉。
+//        if(url == null)
+//            return;
+
+        String address = storeInformation.split(",")[1];
+        new GeoCodingTask().execute(address);
         String url = getIntent().getStringExtra("photoURL");
-        if (url != null)
-        {
-            Picasso.with(this).load(url).into(photo);
+
+        if (url == null)
+            return;
+
+        new ImageLoadingTask(photo).execute(url);
+
+
+//        用Thread這個方法不好
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                byte[] bytes = Utils.urlToBytes(url);
+//                String result = new String(bytes);
+//                Log.d("debug", result);
+//                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//                photo.setImageBitmap(bmp);
+//            }
+//        });
+//        thread.start();
+
+//        改用AsyncTask
+//        new AsyncTask<String, Void, byte[]>()
+//        {
+//            @Override
+//            protected byte[] doInBackground(String... params)
+//            {
+//                String url = params[0];
+//                return Utils.urlToBytes(url);
+//            }
+//
+//            @Override
+//            protected void onPostExecute(byte[] bytes)
+//            {
+//                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//                photo.setImageBitmap(bmp);
+//                super.onPostExecute(bytes);
+//            }
+//
+//        }.execute(url);
+    }
+
+    class GeoCodingTask extends AsyncTask<String, Void, byte[]>
+    {
+//        ImageView imageView;
+//
+//        public GeoCodingTask(ImageView imageView) {
+//            this.imageView = imageView;
+//        }
+
+        private double[] latlng;
+        private String url;
+        @Override
+        protected byte[] doInBackground(String... params) {
+            String address = params[0];
+            latlng = Utils.addressToLatLng(address);
+            url = Utils.getStaticMapUrl(latlng, 17);
+            return Utils.urlToBytes(url);
         }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            webView.loadUrl(url);
+
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            staticMapimageView.setImageBitmap(bmp);
+            super.onPostExecute(bytes);
+        }
+
+
+    }
+
+    class ImageLoadingTask extends AsyncTask<String, Void, byte[]>
+    {
+        ImageView imageView;
+
+        public ImageLoadingTask(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected byte[] doInBackground(String... params) {
+            String url = params[0];
+            return Utils.urlToBytes(url);
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            photo.setImageBitmap(bmp);
+            super.onPostExecute(bytes);
+        }
+
 
     }
 }
--- SimpleUI/app/src/main/java/com/example/simpleui/simpleui/Utils.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/simpleui/Utils.java
@@ -3,6 +3,10 @@ package com.example.simpleui.simpleui;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -12,79 +16,150 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * Created by Festiny on 2016/3/10.
  */
 public class Utils {
-    public static void writeFile(Context context, String fileName, String content)
-    {
-        try{
+    public static void writeFile(Context context, String fileName, String content) {
+        try {
             FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_APPEND);
             fos.write(content.getBytes());
             fos.close();
-        }
-        catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static String readFile(Context context, String fileName)
-    {
-        try{
+    public static String readFile(Context context, String fileName) {
+        try {
             FileInputStream fis = context.openFileInput(fileName);
             byte[] buffer = new byte[1024];
             fis.read(buffer, 0, buffer.length);
             fis.close();
             return new String(buffer);
-        }
-        catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return ""; //return null;
     }
 
-    public static Uri getPhotoUri()
-    {
+    public static Uri getPhotoUri() {
         File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
-        if (dir.exists() == false)
-        {
+        if (dir.exists() == false) {
             dir.mkdir();
         }
         File file = new File(dir, "simple_photo.png");
         return Uri.fromFile(file);
     }
 
-    public static byte[] uriToBytes(Context context, Uri uri)
-    {
-        try
-        {
+    public static byte[] uriToBytes(Context context, Uri uri) {
+        try {
             InputStream is = context.getContentResolver().openInputStream(uri);
-            ByteArrayOutputStream baso = new ByteArrayOutputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
             byte[] buffer = new byte[1024];
             int len = 0;
-            while ((len = is.read(buffer)) != -1)
-            {
-                baso.write(buffer, 0, len);
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
             }
 
-            return baso.toByteArray();
-        }
-        catch (FileNotFoundException e) {
+            return baos.toByteArray();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    public static byte[] urlToBytes(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+
+            return baos.toByteArray();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String getGeoCodingUrl(String address) {
+        try {
+            address = URLEncoder.encode(address, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address;
+        return url;
+    }
+
+    public static double[] getLatLngFromJsonString(String jsonstring)
+    {
+        try {
+            JSONObject object = new JSONObject(jsonstring);
+
+            if (!object.getString("status").equals("OK"))
+                return null;
+
+            JSONObject location =  object.getJSONArray("results")
+                    .getJSONObject(0)
+                    .getJSONObject("geometry")
+                    .getJSONObject("location");
+
+            double lat = location.getDouble("lat");
+            double lng = location.getDouble("lng");
+
+            return new double[]{lat , lng};
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+
+    public static double[] addressToLatLng(String address)
+    {
+        String url = Utils.getGeoCodingUrl(address);
+        byte[] bytes = Utils.urlToBytes(url);
+        String result = new String(bytes);
+        double[] locations = Utils.getLatLngFromJsonString(result);
+
+        return locations;
+    }
+
+    public static String getStaticMapUrl(double[] latlng, int zoom)
+    {
+        String center = latlng[0] + "," + latlng[1];
+        String url = "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=" + zoom + "&size=640x400";
+        return url;
+
     }
 }
